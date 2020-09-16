57df9d46f5bb644384d69ec43dee4da83bee91b9
Carlos VP <ca.varaperez@gmail.com>
2020-09-15 20:04:15 -0500

    Perceptron is now working!

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+import weakref
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -13,6 +14,7 @@ class Perceptron(object):
         self.current_epoch = 0
         self.epoch_limit = epoch_limit
         self.weights = np.random.rand(1, 3)
+        self.slopes = []
 
     def activation(self, X):
         if(np.dot(self.weights, np.array(X)) >= 0):
@@ -27,50 +29,40 @@ class Perceptron(object):
         while(not_done and (self.current_epoch <= self.epoch_limit)):
             self.current_epoch += 1
             not_done = False
+            x = np.arange(0, 8)
             for element in self.data:
                 error = element['expected'] - self.activation(element['coord'])
                 if(error != 0):
                     not_done = True
                     self.error_freq += 1
                     self.adjust(error, element['coord'])
+            self.slopes.append(self.weights[0][1])
+
         self.trained = True
-        # n_samples = X.shape[0]
-        # n_features = X.shape[1]
-
-        # # Add 1 for the bias term
-        # self.weights = np.zeros((n_features+1,))
-
-        # # Add column of 1s
-        # X = np.concatenate([X, np.ones((n_samples, 1))], axis=1)
-
-        # for i in range(n_iter):
-        #     for j in range(n_samples):
-        #         if y[j]*np.dot(self.weights, X[j, :]) <= 0:
-        #             self.weights += y[j]*X[j, :]
-
-
-def load_data():
-    URL_ = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-    data = pd.read_csv(URL_, header=None)
-    print(data)
-
-    # make the dataset linearly separable
-    data = data[:100]
-    data[4] = np.where(data.iloc[:, -1] == 'Iris-setosa', 0, 1)
-    data = np.asmatrix(data, dtype='float64')
-    return data
 
 
 if __name__ == "__main__":
     with open('perceptron/bulk_data.json', 'r') as json_file:
         data = json.load(json_file)
 
+    perceptron = Perceptron(data, eta=0.4, epoch_limit=200)
+    perceptron.process()
+
     for element in data:
+        sym = 'ro'
         if(element['expected'] == 1):
-            plt.plot(element['coord'][1], element['coord'][2], 'bo')
-        else:
-            plt.plot(element['coord'][1], element['coord'][2], 'ro')
-    perceptron = Perceptron(data, eta=0.5, epoch_limit=100)
-    y = np.array
-    print(perceptron.weights)
-    plt.show()
+            sym = 'bo'
+        plt.plot(element['coord'][1], element['coord'][2], sym)
+
+    # The line drawing must be re-done because of the origin of the line
+    axes = plt.axis()
+    while(perceptron.slopes):
+        slope = perceptron.slopes.pop()
+        current_line = plt.axline(
+            (axes[0]-.2, axes[2]-.2), slope=slope, color="orange")
+        plt.pause(0.001)
+        wl = weakref.ref(current_line)
+        current_line.remove()
+        del current_line
+
+    input()
