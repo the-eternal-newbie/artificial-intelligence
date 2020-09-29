c32bdb34f6257ed3539dd377a0bea8c256acd1e6
Carlos VP <ca.varaperez@gmail.com>
2020-09-29 14:44:01 -0500

    Adding better UX to the GUI & new buttons

--- adaline/main.py
+++ adaline/main.py
@@ -1,28 +1,35 @@
-import json
 import sys
+sys.path.insert(
+    0, "/home/carlosvp/dev/school/ia_ii/artificial-intelligence/perceptron")
+import json
 import weakref
 import numpy as np
+import tkinter as tk
+
 from os import path
 from os import execl
 from adaline import Adaline
-import tkinter as tk
 from tkinter import messagebox
+from perceptron import Perceptron
 from matplotlib.figure import Figure
 from matplotlib.ticker import MaxNLocator
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.backend_bases import key_press_handler
 
+
 # ! This GUI evolves frome the one implemented on the Perceptron project
 
 root = tk.Tk()
-root.wm_title("HW1 - Perceptron")
+root.wm_title("HW2 - Adaline & Perceptron")
+root.geometry("700x765")
+root.resizable(False, False)
 # Everytime the program is executed, a new file is created, replacing the previous one to erase previous data
 file = open('bulk_data.json', 'w')
 file.write('[]')  # Writes a json list element
 file.close()
 global quit_button
-global train_button
+global perceptron_button
 global weights_button
 global refresh_button
 global weights
@@ -42,20 +49,16 @@ class Layout(object):
         self.ax.axhline(y=0, color="black")
         self.ax.axvline(x=0, color="black")
         # Draw arrow points
-        self.ax.scatter(0, size-.1, marker='^', color='black')
-        self.ax.scatter(size-.1, 0, marker='>', color='black')
-        self.ax.scatter(0, -size+.1, marker='v', color='black')
-        self.ax.scatter(-size+.1, 0, marker='<', color='black')
+        self.ax.scatter(0, size - .1, marker='^', color='black')
+        self.ax.scatter(size - .1, 0, marker='>', color='black')
+        self.ax.scatter(0, -size + .1, marker='v', color='black')
+        self.ax.scatter(-size + .1, 0, marker='<', color='black')
 
         # Connect the plot with the GUI interface
         self.canvas = FigureCanvasTkAgg(fig, master=root)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.NONE, expand=False)
         self.canvas.mpl_connect("button_press_event", self.on_click)
-
-        toolbar = NavigationToolbar2Tk(self.canvas, root)
-        toolbar.update()
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
     # Left click -> Class with 0s
     # Right click -> Class with 1s
@@ -72,7 +75,7 @@ class Layout(object):
             self.ax.scatter(point['coord'][1],
                             point['coord'][2], color=point['color'])
             self.ax.annotate('Class {}'.format(
-                point['class']), (point['coord'][1]+.5, point['coord'][2]))
+                point['class']), (point['coord'][1] + .5, point['coord'][2]))
             self.canvas.draw()  # Refreshes the canvas
         else:
             if(ix != None):
@@ -119,6 +122,7 @@ class ErrorLayout(object):
 def window_error(epoch_amount, error_freq):
     window = tk.Toplevel(root)
     window.geometry("700x800+900+100")
+    window.resizable(False, False)
     error_view = ErrorLayout(
         window, 'Error', x_limit=epoch_amount, y_limit=max(error_freq))
     x = np.arange(epoch_amount)
@@ -133,7 +137,7 @@ def _quit():
 
 
 def _initialize_weights(w0_field, w1_field, w2_field):
-    train_button.config(state='normal')
+    perceptron_button.config(state='normal')
     weights_button.config(state='disabled')
     try:
         weights = [float(w0_field.get()), float(
@@ -142,18 +146,18 @@ def _initialize_weights(w0_field, w1_field, w2_field):
         x = np.linspace(-5, 5, 100)
         if(weights[2] == 0):
             raise ValueError
-        y = (weights[0] - (weights[1]*x))/weights[2]
+        y = (weights[0] - (weights[1] * x)) / weights[2]
         layout.ax.plot(x, y, color='blue')
         layout.canvas.draw()
     except ValueError as error:
-        train_button.config(state='disabled')
+        perceptron_button.config(state='disabled')
         weights_button.config(state='normal')
         messagebox.showerror(
             error, 'Weight values must be different from zero!')
 
 
 # Starts the perceptron algorithm
-def _train(eta_field, epoch_field):
+def _train(eta_field, epoch_field, type='perceptron'):
     try:
         eta = float(eta_field.get())
         epoch_limit = int(epoch_field.get())
@@ -170,7 +174,11 @@ def _train(eta_field, epoch_field):
             args['epoch_limit'] = epoch_limit
 
         try:
-            trainer = Adaline(**args)
+            if(type == 'perceptron'):
+                trainer = Perceptron(**args)
+            else:
+                trainer = Adaline(**args)
+
             trainer.process()
             layout.weights = trainer.weights
             layout.has_been_trained = True
@@ -182,7 +190,7 @@ def _train(eta_field, epoch_field):
         del l
         x = np.linspace(-5, 5, 100)
         for line in trainer.lines:
-            y = (line[0] - (line[1]*x))/line[2]
+            y = (line[0] - (line[1] * x)) / line[2]
             lines = layout.ax.plot(x, y, color='blue')
             l = lines.pop()
             wl = weakref.ref(l)
@@ -190,7 +198,7 @@ def _train(eta_field, epoch_field):
             l.remove()
             del l
         y = (trainer.weights[0][0] -
-             (trainer.weights[0][1]*x))/trainer.weights[0][2]
+             (trainer.weights[0][1] * x)) / trainer.weights[0][2]
         layout.ax.plot(x, y, color='blue')
         messagebox.showinfo('Perceptron training has finished',
                             'The solution was found in the epoch number {}'.format(trainer.current_epoch))
@@ -213,43 +221,67 @@ if __name__ == "__main__":
     # Creates the eta label and field
     eta_label = tk.Label(root, text='η value:', width=10, anchor=tk.S)
     eta_field = tk.Spinbox(master=root, from_=0, to=5, increment=.1, width=5)
-    eta_field.place(x=85, y=660)
-    eta_label.place(x=1, y=660)
+    eta_field.place(x=85, y=700)
+    eta_label.place(x=1, y=700)
 
     # Creates the epoch limit label and field
     epoch_label = tk.Label(root, text='Epoch limit:', width=10)
     epoch_field = tk.Spinbox(master=root, from_=0,
                              to=4000, increment=100, width=5)
-    epoch_field.place(x=85, y=680)
-    epoch_label.place(x=1, y=680)
+    epoch_field.place(x=85, y=720)
+    epoch_label.place(x=1, y=720)
+
+    # Creates the squared error label and field
+    sqre_label = tk.Label(root, text='Desired ε²:', width=10)
+    sqre_field = tk.Spinbox(master=root, from_=0,
+                             to=10, increment=.1, width=5)
+    sqre_field.place(x=85, y=740)
+    sqre_label.place(x=1, y=740)
 
     # Creates the weights label and each field for the three weights
-    weights_label = tk.Label(root, text='ω0: \t ω1: \t   ω2: ')
+    weights_label = tk.Label(root, text='ω0:\n\nω1:\nω2:')
     w0_field = tk.Spinbox(master=root, from_=0, to=10, increment=.1, width=3)
+    w0_label = tk.Label(root, text='ω₀:')
+    
     w1_field = tk.Spinbox(master=root, from_=0, to=10, increment=.1, width=3)
+    w1_label = tk.Label(root, text='ω₁:')
+    
     w2_field = tk.Spinbox(master=root, from_=0, to=10, increment=.1, width=3)
-    weights_label.place(x=1, y=705)
-    w0_field.place(x=28, y=705)
-    w1_field.place(x=98, y=705)
-    w2_field.place(x=168, y=705)
+    w2_label = tk.Label(root, text='ω₂:')
+    
+    w0_field.place(x=178, y=700)
+    w0_label.place(x=150, y=700)
+    
+    w1_field.place(x=178, y=720)
+    w1_label.place(x=150, y=720)
+    
+    w2_field.place(x=178, y=740)
+    w2_label.place(x=150, y=740)
+    
     weights = []    # declares the weights variable in the global scope of the program
 
-    # Creates three buttons (to initialize the weight vector, to start the perceptron's training and to quit the program)
+    # * Creates three buttons (to initialize the weight vector, to start the perceptron's training and to quit the program)
     quit_button = tk.Button(master=root, text='Quit', command=_quit)
-    quit_button.pack(side=tk.RIGHT)
+    quit_button.place(x=645, y=730)
 
     refresh_button = tk.Button(master=root, text='Restart', command=_refresh)
-    refresh_button.pack(side=tk.RIGHT)
+    refresh_button.place(x=575, y=730)
 
     weights_button = tk.Button(
         master=root, text='Initialize weights', command=lambda: _initialize_weights(w0_field, w1_field, w2_field))
-    weights_button.pack(side=tk.RIGHT)
+    weights_button.place(x=445, y=730)
 
-    train_button = tk.Button(
-        master=root, text='Start Trainning', command=lambda: _train(eta_field, epoch_field))
-    train_button.pack(side=tk.RIGHT)
+    # * Training buttons
+    perceptron_button = tk.Button(
+        master=root, text='Start Perceptron Trainning', command=lambda: _train(eta_field, epoch_field, 'perceptron'))
+    perceptron_button.place(x=326, y=700)
 
-    # Disables the train_button until the weight_button is pressed
-    train_button.config(state='disabled')
+    adaline_button = tk.Button(
+        master=root, text='Start Adaline Trainning', command=lambda: _train(eta_field, epoch_field, 'adaline'))
+    adaline_button.place(x=523, y=700)
+
+    # Disables the perceptron & adaline buttons until the weight_button is pressed
+    perceptron_button.config(state='disabled')
+    adaline_button.config(state='disabled')
     refresh_button.config(state='disabled')
     tk.mainloop()
